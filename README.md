ac6ef80248d6ffe1b5b043843be81988673019f1
Clayton Marinho <claytonmarinho@gmail.com>
2016-10-24 12:51:21 -0200

    run.sh improvements

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # letsencrypt-docker-snippet
 Boilerplate for automatic certificate w/multiple domains
+
+Type ./run.sh
+
+*** read more ***
+https://github.com/JrCs/docker-letsencrypt-nginx-proxy-companion
--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
+DATA_PATH="$HOME/.nginxdata";
+
 docker run -d -p 80:80 -p 443:443 \
     --name nginx \
-    -v $HOME/.nginxdata/conf.d:/etc/nginx/conf.d  \
-    -v $HOME/.nginxdata/vhost.d:/etc/nginx/vhost.d \
-    -v $HOME/.nginxdata/html:/usr/share/nginx/html \
-    -v $HOME/.nginxdata/certs:/etc/nginx/certs:ro \
+    --restart always \
+    -v $DATA_PATH/conf.d:/etc/nginx/conf.d  \
+    -v $DATA_PATH/vhost.d:/etc/nginx/vhost.d \
+    -v $DATA_PATH/html:/usr/share/nginx/html \
+    -v $DATA_PATH/certs:/etc/nginx/certs:ro \
     nginx
 
 docker run -d \
@@ -13,3 +16,12 @@ docker run -d \
     -v /var/run/docker.sock:/tmp/docker.sock:ro \
     jwilder/docker-gen \
     -notify-sighup nginx -watch -only-exposed -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
+
+docker run -d \
+    --name nginx-letsencrypt \
+    --restart always \
+    -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" \
+    --volumes-from nginx \
+    -v $DATA_PATH/:/etc/nginx/certs:rw \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    jrcs/letsencrypt-nginx-proxy-companion
