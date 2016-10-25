5bc7786d5a41ad3ba32298ff07c3c88c9e378574
Clayton Marinho <claytonmarinho@gmail.com>
2016-10-25 00:13:00 -0200

    simplifying

--- run.sh
+++ run.sh
@@ -5,15 +5,15 @@
 #
 
 DATA_PATH="$HOME/.nginxdata"; #default place to save container data
-NGINX_CONTAINER='nginx-base';
-NGINX_GEN_CONTAINER='nginx-gen';
-NGINX_LETSENCRYPT_CONTAINER='nginx-letsencrypt';
+NGINX_CONTAINER='nginx-proxy';
+#NGINX_GEN_CONTAINER='nginx-gen';
+#NGINX_LETSENCRYPT_CONTAINER='nginx-letsencrypt';
 
 
 mkdir $DATA_PATH;
 
 #getting the latest nginx.tmpl
-curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > $DATA_PATH/nginx.tmpl
+#curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > $DATA_PATH/nginx.tmpl
 
 #
 # NGINX CONTAINER
@@ -30,33 +30,33 @@ fi
 docker run -d -p 80:80 -p 443:443 \
     --name $NGINX_CONTAINER \
     --restart always \
-    -v $DATA_PATH/conf.d:/etc/nginx/conf.d  \
+    -v $DATA_PATH/certs:/etc/nginx/certs:ro \
     -v $DATA_PATH/vhost.d:/etc/nginx/vhost.d \
     -v $DATA_PATH/html:/usr/share/nginx/html \
-    -v $DATA_PATH/certs:/etc/nginx/certs:ro \
-    nginx
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    nginx-proxy
 
 #
 # NGINX-GEN CONTAINER
 #
 
 #checking if there´s a container with the same name
-NGINX_GEN_CHECK=$(docker ps --filter="name=$NGINX_GEN_CONTAINER" -q -a);
+# NGINX_GEN_CHECK=$(docker ps --filter="name=$NGINX_GEN_CONTAINER" -q -a);
 
-if [ $NGINX_GEN_CHECK ]; then
-  docker rm -f $NGINX_GEN_CHECK
-fi
+# if [ $NGINX_GEN_CHECK ]; then
+#   docker rm -f $NGINX_GEN_CHECK
+# fi
 
 #running container
-docker run -d \
-    --name $NGINX_GEN_CONTAINER \
-    --restart always \
-    --volumes-from $NGINX_CONTAINER \
-    -v $DATA_PATH/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
-    -v /var/run/docker.sock:/tmp/docker.sock:ro \
-    --expose 8011 \
-    jwilder/docker-gen \
-    -notify-sighup nginx -watch -only-exposed -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
+# docker run -d \
+#     --name $NGINX_GEN_CONTAINER \
+#     --restart always \
+#     --volumes-from $NGINX_CONTAINER \
+#     -v $DATA_PATH/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
+#     -v /var/run/docker.sock:/tmp/docker.sock:ro \
+#     --expose 8011 \
+#     jwilder/docker-gen \
+#     -notify-sighup nginx -watch -only-exposed -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
 
 #
 # NGINX-LETSENCRYPT CONTAINER
@@ -73,7 +73,6 @@ fi
 docker run -d \
     --name $NGINX_LETSENCRYPT_CONTAINER \
     --restart always \
-    -e "NGINX_DOCKER_GEN_CONTAINER=$NGINX_GEN_CONTAINER" \
     --volumes-from $NGINX_CONTAINER \
     -v $DATA_PATH/certs:/etc/nginx/certs:rw \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
