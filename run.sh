c7ec18fd2840ad806061c8728ebdcf726a9e9c7f
Clayton Marinho <claytonmarinho@gmail.com>
2016-10-25 00:33:44 -0200

    cleaning code

--- run.sh
+++ run.sh
@@ -6,14 +6,9 @@
 
 DATA_PATH="$HOME/.nginxdata"; #default place to save container data
 NGINX_CONTAINER='nginx-proxy';
-#NGINX_GEN_CONTAINER='nginx-gen';
 NGINX_LETSENCRYPT_CONTAINER='nginx-letsencrypt';
 
-
 mkdir $DATA_PATH;
-
-#getting the latest nginx.tmpl
-#curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > $DATA_PATH/nginx.tmpl
 
 #
 # NGINX CONTAINER
@@ -35,28 +30,6 @@ docker run -d -p 80:80 -p 443:443 \
     -v $DATA_PATH/html:/usr/share/nginx/html \
     -v /var/run/docker.sock:/tmp/docker.sock:ro \
     jwilder/nginx-proxy
-
-#
-# NGINX-GEN CONTAINER
-#
-
-#checking if there´s a container with the same name
-# NGINX_GEN_CHECK=$(docker ps --filter="name=$NGINX_GEN_CONTAINER" -q -a);
-
-# if [ $NGINX_GEN_CHECK ]; then
-#   docker rm -f $NGINX_GEN_CHECK
-# fi
-
-#running container
-# docker run -d \
-#     --name $NGINX_GEN_CONTAINER \
-#     --restart always \
-#     --volumes-from $NGINX_CONTAINER \
-#     -v $DATA_PATH/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
-#     -v /var/run/docker.sock:/tmp/docker.sock:ro \
-#     --expose 8011 \
-#     jwilder/docker-gen \
-#     -notify-sighup nginx -watch -only-exposed -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
 
 #
 # NGINX-LETSENCRYPT CONTAINER
